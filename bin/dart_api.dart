98e6632bc3aa2fa6593cb797c4627829e20b2952
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-10-22 16:40:05 +0500

    done

--- bin/dart_api.dart
+++ bin/dart_api.dart
@@ -1,5 +1,15 @@
-import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:io';
 
-void main() {
-  print('Hi');
+void main() async {
+  http.Response response = await http.get(
+    Uri.parse('https://randomuser.me/api/'),
+  );
+
+  Map json = jsonDecode(response.body);
+  List res = json['results'];
+
+  print(res[0]['name']['first']);
+  print(res[0]['name']['last']);
 }
